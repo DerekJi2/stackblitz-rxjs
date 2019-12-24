4b8c18d71223359727b8f243effa2a9ff92d4197
DerekToop <syfool@hotmail.com>
2019-12-25 09:57:54 +1030

    page91()

--- chapter2.ts
+++ chapter2.ts
@@ -1,8 +1,35 @@
-import { of } from 'rxjs'; 
+import { of, timer } from 'rxjs'; 
 import { Observable } from 'rxjs/Observable';
 
 export class Chapter2 {
+  /**
+   * 
+   */
+  public page91() {
+    console.log('Page 91 ...');
 
+    const onSubscribe = observer => {
+      let data = 1;
+      const interval = setInterval(() => {
+        observer.next(data++);
+        if (data > 3) {
+          clearInterval(interval);
+        }        
+      }, 1000);
+    };
+
+    const source$ = new Observable(onSubscribe);
+
+    const theObserver = {
+      next: item => console.log(item)
+    };
+
+    source$.subscribe(theObserver);
+  }
+
+  /**
+   * 
+   */
   public page80() {
     console.log('Page 80 ...');
     const source$ = of(1, 2, 3);
@@ -10,13 +37,16 @@ export class Chapter2 {
     
   }
   
+  /**
+   * 
+   */
   public page85() {
     console.log('Page 85 ...');
 
     const onSubscribe = observer => {
       observer.next(1);
       observer.next(2);
-      observer.next(6);
+      observer.next(3);
     };
 
     const source$ = new Observable(onSubscribe);
